f4bbea4bcd11e0b0824b60937a5643f0b5ae2513
asrospie <asrospierski@gmail.com>
2021-08-10 00:26:07 -0400

    Update settings

--- tales_from_aberath/tales_from_aberath/settings.py
+++ tales_from_aberath/tales_from_aberath/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ '127.0.0.1', 'localhost', 'tales-from-aberath.herokuapp.com']
+ALLOWED_HOSTS = [ '127.0.0.1', 'localhost', 'tales-from-aberath.herokuapp.com', 'https://tales-from-aberath.herokuapp.com']
 
 
 # Application definition
